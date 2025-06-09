48a1516fe307690e5c04960a232f01075fbf9fb0
Chainchinjung <upawongnatthawut@gmail.com>
2025-06-09 16:26:39 +0700

    test commit with test branch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import { config } from "@dotenvx/dotenvx"
 import { serve } from "@hono/node-server"
 import { Hono } from "hono"
+import { cors } from "hono/cors"
 import { setupOpenApi } from "./config/openapi/setup-openapi.js"
 import { setupScalarDocs } from "./config/openapi/setup-scalar-docs.js"
 import * as AdminOrganizationController from "./controllers/adminorganizations/index.js"
@@ -16,6 +17,12 @@ import * as UserController from "./controllers/users/index.js"
 // config()
 
 const app = new Hono()
+app.use(cors({
+  allowHeaders: ["Content-Type", "Authorization"],
+  allowMethods: ["GET", "HEAD", "PUT", "POST", "DELETE", "PATCH"],
+  credentials: true,
+  origin: ["http://localhost:5173"],
+}))
 setupOpenApi(app)
 
 app.route("/admin/user", AdminUserController.setupUserByAdminRoutes())
@@ -27,7 +34,6 @@ app.route("/users", UserController.setupUserRoutes())
 app.route("/organization", ORGController.setupOrganizationRoutes())
 app.route("/project", ProjectController.setupProjectRoutes())
 app.route("/refreshToken", RefreshTokenController.setupRefreshTokenRoutes())
-
 
 app.route("/docs", setupScalarDocs())
 app.route("/healthz", healthzApp)
